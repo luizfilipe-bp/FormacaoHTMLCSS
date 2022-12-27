1c264dfe3c283957ce31dc626a29bfbb26667b44
Luiz Filipe Bartelega Penha <luizfpenha16@gmail.com>
2022-12-26 21:00:11 -0300

    Adicionado projeto sobre flexbox e grid

---
+++ flex-e-grid-main/index.js
@@ -0,0 +1,7 @@
+const botaomenu = document.querySelector('.cabecalho__menu')
+const menu = document.querySelector(".menu-lateral")
+
+botaomenu.addEventListener("click", () => {
+    menu.classList.toggle("menu-lateral--ativo")
+
+})
